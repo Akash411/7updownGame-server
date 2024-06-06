b6add3a30a9fd5d9caaaf85302ebe4cee44bccc6
Akash <a3328307@gmail.com>
2024-06-06 14:19:59 +0530

    get command added to test

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.post("/roll-dice", (req, res) => {
   const die1 = Math.floor(Math.random() * 6) + 1;
